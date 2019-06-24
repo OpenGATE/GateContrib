78bb399a16e23e06aeaee21170f63dfa540eb318
tbaudier <thomas.baudier@creatis.insa-lyon.fr>
2019-06-24 17:22:06 +0200

    To create the trace of the NN, generate random values instead of taking real inputs The results are identical

--- imaging/garf/createTrace.py
+++ imaging/garf/createTrace.py
@@ -2,19 +2,13 @@ import torch
 import garf
 from torch.autograd import Variable
 import json
+import numpy as np
 
 nn, model = garf.load_nn("/home/tbaudier/david/gateTorch/gateContribExample/pth/arf.pth")
 
-data_filename="/home/tbaudier/david/gateTorch/gateContribExample/output_arf_dataset/test.root"
-data, x, y, theta, phi, E = garf.load_test_dataset(data_filename)
-
 model_data = nn['model_data']
-ax = data[:, 2:5]
-tx = ax[0:400]
 
-x_mean = model_data['x_mean']
-x_std = model_data['x_std']
-tx = (tx - x_mean) / x_std
+tx = np.random.rand(1,3) # generate 3 random values between 0 and 1
 dtypef = torch.cuda.FloatTensor
 model.cuda()
 tx = tx.astype('float32')
